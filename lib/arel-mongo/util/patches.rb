925d3d9466ef61d5bbe9ed6ba9fdb18139bde410
akdubya <alekswilliams@earthlink.net>
2010-04-30 03:55:21 -0400

    Speed up hash manipulation

--- lib/arel-mongo/util/patches.rb
+++ lib/arel-mongo/util/patches.rb
@@ -3,7 +3,7 @@ module Arel
     def bind(relation)
       inject({}) do |bound, (key, value)|
         value = Hash === value ? Arel::Value.new(value, relation) : value.bind(relation)
-        bound.merge(key.bind(relation) => value)
+        bound.merge!(key.bind(relation) => value)
       end
     end
   end
